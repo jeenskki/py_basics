179e415293fe9e5eb245f030b9244d0034baac12
Jinseok Kim <jeenskki@Jinseokui-MacBookPro.local>
2020-06-24 15:15:38 +0900

    20/06/24 updated

--- exercises/ex11.py
+++ exercises/ex11.py
@@ -1,13 +1,41 @@
-a = []
-for i in range (10):
-    a.append(i)
-print(a)
+import re
+p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
-b = [i for i in range(10)]
-print(b)
+email = 'kim@abc.com'
 
-for i in range(10):
-    if (1 % 2) == 0:
-        print("짝수")
+print(p.match(email) != None)
+# a = []
+# for i in range (10):
+#     a.append(i)
+# print(a)
+
+# b = [i for i in range(10)]
+# print(b)
+
+# for i in range(10):
+#     if (1 % 2) == 0:
+#         print("짝수")
         
-c = [i for i in range(10) if 1 % 2 == 0]
+# c = [i for i in range(10) if 1 % 2 == 0]
+# dic = {"a":30, "b":20, "c":30}
+# tmp_list = ["1","2","3","4","5"]
+
+# for i, val in enumerate(tmp_list):
+#     print(i,val)
+    
+# for i in dic:
+#     print(dic.get(i))
+# for k, v in dic.items():
+#     print(k,v)
+
+a = 'avbc@'
+b = '1234'
+c = '한글'
+print(a.isalpha())
+print(a.isalnum())
+print(b.isdigit())
+print(c.isalnum())
+print(c.isalpha())
+print(c.isascii())
+
+# print(a.find("@"))
